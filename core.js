6b230622d1360abfb977b0ec491b04e8b9890bf5
orl0pl <orlyyt@int.pl>
2023-01-13 17:14:27 +0100

    probably finished api routes

--- core.js
+++ core.js
@@ -10,38 +10,122 @@ function searchContainerById(json, id) {
 
 	return null;
 }
+function addContainer(json /*DB Data*/, parentId /*uuid of the parent container*/, newContainer /*new container object*/) {
+	//Add container
+	if (!json || !parentId || !newContainer) return json;
+
+	if (json.id === parentId) {
+		json.containers.push(newContainer);
+		return json;
+	}
+
+	for (const container of json.containers) {
+		addContainer(container, parentId, newContainer);
+	}
+
+	return json;
+}
+function moveContainer(global_json,json /*DB Data*/,  thisId /*uuid of the container you want to move*/, newParentId /*uuid of the new container object*/) {
+	//Add container
+	if (!json || !thisId || !newParentId) return json;
+	var thisContainer = searchContainerById(global_json, thisId)
+	console.log("INIT---------",thisContainer)
+	if(thisContainer !== null && thisContainer !== undefined){
+		console.log("THIS CONTAINER GUT",json.id)
+		if (json.id === newParentId) {
+			
+			console.log("FINDED", json)
+			json.containers.push(thisContainer);
+			console.log("PUSHED", json.containers)
+			deleteContainerById(global_json, thisId);
+			return json;
+		}
+		else{
+			for (const container of json.containers) {
+				moveContainer(global_json,container, thisId, newParentId);
+			}
+		}
+	
+		return json;
+	}
+	else {
+		console.log("NULL ERROR",thisContainer)
+		return null
+	}
+	
+}
+function editContainerAttrById(json, id, attr){
+	//Add container
+	if (!json || !id|| !attr) return json;
+	if (json.id === id) {
+		json.attr = attr;
+		return json;
+	}
+	else {
+		for (const container of json.containers) {
+			editContainerAttrById(container, id, attr);
+		}
+	}
+
+	return json;
+}
+function deleteContainerById(json, id) {
+	if (!json || !id) return json;
+  
+	if (json.id === id) {
+	  return null;
+	}
+  
+	for (let i = 0; i < json.containers.length; i++) {
+	  const container = json.containers[i];
+	  const result = deleteContainerById(container, id);
+	  if (result === null) {
+		json.containers.splice(i, 1);
+		return json;
+	  }
+	}
+  
+	return json;
+  }
+  
 function searchContainerByCompareFnArray(json, key, compareFns) {
 	/*
 	This function searches
 	*/
 	if (!json || !key || !compareFns || !compareFns.length) return [];
-  
+
 	let result = [];
 	if (json.attr && compareFns.every((compareFn) => compareFn(json.attr[key]))) {
-	  result.push(json);
+		result.push(json);
 	}
-  
+
 	for (const container of json.containers) {
-	  result = result.concat(searchContainerByComapreFnArray(container, key, compareFns));
+		result = result.concat(searchContainerByComapreFnArray(container, key, compareFns));
 	}
-  
+
 	return result;
-  }
-  
-function searchContainerByCompareFn(json, key, compareFn) {
-	/*
-	This function searches 
-	*/
+}
+
+function searchContainerByCompareFn(json /* db data */, key /* key to search */, compareFn /*Compiration functunction eg. `(x)=>x>2`*/) {
+	//Search Containers
 	if (!json || !key || !compareFn) return [];
-  
+
 	let result = [];
 	if (json.attr && compareFn(json.attr[key])) result.push(json);
-  
+
 	for (const container of json.containers) {
-	  result = result.concat(searchContainerByCompareFn(container, key, compareFn));
+		result = result.concat(searchContainerByCompareFn(container, key, compareFn));
 	}
-  
+
 	return result;
-  }
-var core = {searchContainerByCompareFn: searchContainerByCompareFn, searchContainerByCompareFnArray: searchContainerByCompareFnArray, searchContainerById: searchContainerById}
+}
+var core = {
+	searchContainerByCompareFn: searchContainerByCompareFn,
+	searchContainerByCompareFnArray: searchContainerByCompareFnArray,
+	searchContainerById: searchContainerById,
+	addContainer: addContainer,
+	deleteContainerById: deleteContainerById,
+	moveContainer: moveContainer,
+	editContainerAttrById, editContainerAttrById
+}
 module.exports = core;
--- index.js
+++ index.js
@@ -1,61 +1,163 @@
 const express = require('express')
 const app = express()
 const port = 1237
-
+const { v4: uuidv4 } = require('uuid');
 
 var jp = require('jsonpath');
 var fs = require('fs');
 var db = JSON.parse(fs.readFileSync('db.json'))
 var core = require('./core');
-var securekey = 'beti35'
-var json = db[0];
-var search = 'bobo'
+app.use(express.json())
 function greaterThan3(name) {
 	return typeof name === "string" && name.length > 3;
 }
-function getKeyPathById(json, id, path = []) {
+function getPathById(json, id, path = []) {
 	if (!json || !id) return [];
-  
+
 	if (json.id === id) return path;
-  
+
 	for (const container of json.containers) {
-	  const result = getPathById(container, id, [...path, json.id]);
-	  if (result.length) return result;
+		const result = getPathById(container, id, [...path, json.id]);
+		if (result.length) return result;
 	}
-  
+
 	return [];
-  }
-function getKeyPathById(json, id, path = [], key) {
-	if (!json || !id) return [];
-  
+}
+function getKeyPathById(json, id, key, path = []) {
+	if (!json || !id || !key) return [];
+
 	if (json.id === id) return path;
-  
+
 	for (const container of json.containers) {
-	  const result = getPathById(container, id, [...path, json.attr[key]], key);
-	  if (result.length) return result;
+		if (container.attr && container.attr[key]) {
+			const result = getKeyPathById(container, id, key, [...path, container.attr[key]]);
+			if (result.length) return result;
+		}
 	}
-  
+
 	return [];
-  }
-  
+}
+
 app.get('/', (req, res) => {
-	res.send('use key')
+	//TODO: LOGIN PAGE ==> ADDS SCRIPT TO SAVE KEY COOKIE AND RECIDECT TO HOME PAGE
+	res.status(401).send({ 'error': 'Use Key' })
 })
-app.get('/:key', (req, res) => {
-	req.params.key === securekey ? res.send(json) : res.sendStatus(403) 
+app.get('/:akey', (req, res) => {
+	const object = db.find(obj => obj.key === req.params.akey);
+	object !== undefined ? res.send(object) : res.send({ 'error': 'Wrong Key' })
 })
-app.get('/:key/id/:id', (req, res) => {
-	req.params.key === securekey ? res.send(core.searchContainerById(json, req.params.id)) : res.sendStatus(403) 
+app.get('/:akey/path/:id', (req, res) => {
+	const object = db.find(obj => obj.key === req.params.akey);
+	object !== undefined ? res.send(getPathById(object, req.params.id)) : res.sendStatus(403)
 })
-app.get('/:key/search/:skey/:fn', (req, res) => {
+app.get('/:akey/keypath/:id/:skey', (req, res) => {
+	const object = db.find(obj => obj.key === req.params.akey);
+	object !== undefined ? res.send(getKeyPathById(object, req.params.id, req.params.skey)) : res.sendStatus(403)
+})
+app.get('/:akey/id/:id', (req, res) => {
+	const object = db.find(obj => obj.key === req.params.akey);
+	object !== undefined ? res.send(core.searchContainerById(object, req.params.id)) : res.sendStatus(403)
+})
+app.get('/:akey/search/:skey/:fn', (req, res) => {
 	var fn = eval(req.params.fn)
-	req.params.key === securekey ? res.send(core.searchContainerByCompareFn(json, req.params.skey, fn)) : res.sendStatus(403) 
+	const object = db.find(obj => obj.key === req.params.akey);
+	object !== undefined ? res.send(core.searchContainerByCompareFn(object, req.params.skey, fn)) : res.sendStatus(403)
 })
-app.get('/:key/multi/:skey/:fn', (req, res) => {
+app.get('/:akey/multi/:skey/:fn', (req, res) => {
 	var fn = eval(req.params.fn)
 	console.log(req.params.fn)
-	req.params.key === securekey ? res.send(core.searchContainerByCompareFn(json, req.params.skey, fn)) : res.sendStatus(403) 
+	const object = db.find(obj => obj.key === req.params.akey);
+	object !== undefined ? res.send(core.searchContainerByCompareFn(object, req.params.skey, fn)) : res.sendStatus(403)
 })
+app.post('/:akey/add/:id', (req, res) => {
+	//console.log(req.body)
+	//res.send('aaaaaaaa')
+	const object = db.find(obj => obj.key === req.params.akey);
+	var newContainer = {
+		id: uuidv4(),
+		attr: {},
+		containers: []
+	};
+	newContainer.attr = { ...req.body };
+	if (object !== undefined) {
+		core.addContainer(object, req.params.id, newContainer)
+		db[db.findIndex(obj => obj.key === req.params.akey)] = object;
+		save()//fs.writeFileSync('db.json', JSON.stringify(db))
+		res.send({
+			status: 'success',
+			newContainer: newContainer,
+			id: newContainer.id
+		})
+	}
+	else {
+		res.sendStatus(401).send({ status: 'Bad Key' })
+	}
+})
+app.delete('/:akey/delete/:id', (req, res) => {
+	var object = db.find(obj => obj.key === req.params.akey);
+	if (object !== undefined) {
+		var object = core.deleteContainerById(object, req.params.id)
+		db[db.findIndex(obj => obj.key === req.params.akey)] = object;
+		save()//fs.writeFileSync('db.json', JSON.stringify(db))
+		res.send({
+			status: 'OK'
+		})
+	}
+	else {
+		res.sendStatus(401).send({ status: 'Bad key' })
+	}
+})
+app.put('/:akey/move/:id/:newparentid', (req, res) => {
+	var object = db.find(obj => obj.key === req.params.akey);
+	if (object !== undefined) {
+		var object = core.moveContainer(object, object, req.params.id, req.params.newparentid);
+		if (object !== null) {
+			db[db.findIndex(obj => obj.key === req.params.akey)] = object;
+			save()//fs.writeFileSync('db.json', JSON.stringify(db))
+			res.send({
+				status: 'OK',
+				moved: req.params.id,
+				to: req.params.newparentid
+			})
+		}
+		else {
+			res.send({
+				status: 'ERROR',
+				description: 'An error occurred while moving item',
+				moved: req.params.id,
+				to: req.params.newparentid
+			})
+		}
+
+	}
+	else {
+		res.sendStatus(401).send({ status: 'Bad key' })
+	}
+})
+app.put('/:akey/edit/:id', (req, res) => {
+	var object = db.find(obj => obj.key === req.params.akey);
+	var attr = req.body;
+	if (object !== undefined) {
+		var object = core.editContainerAttrById(object, req.params.id, attr)
+		db[db.findIndex(obj => obj.key === req.params.akey)] = object;
+		save()//fs.writeFileSync('db.json', JSON.stringify(db))
+		res.send({
+			status: 'success',
+			id: req.params.id
+		})
+	}
+	else {
+		res.sendStatus(401).send({ status: 'Bad Key' })
+	}
+})
+function save() {
+	fs.writeFileSync('db.json', JSON.stringify(db, null, 2))
+}
+app.get('/:akey/qr/', (req, res) => {
+	const object = db.find(obj => obj.key === req.params.akey);
+	object !== undefined ? res.send('https://quickchart.io/qr?text=' + req.params.key + '&light=0000&dark=09f&size=200&format=svg') : res.send({ 'error': 'Wrong Key' })
+})
+
 app.listen(port, () => {
-	console.log(`Example app listening on localhost:${port}/`)
+	console.log(`Example app listening on http://localhost:${port}`)
 })
